bc175f334ec9b4358a0d563e672d1639820a4848
Elaine Ou <elaine.ou@gmail.com>
2015-11-01 15:27:24 -0800

    don't use scheduler

--- Pages/bot.py
+++ Pages/bot.py
@@ -1,7 +1,6 @@
 import random
 from TwitterAPI import TwitterAPI
 from stringdefs import *
-from apscheduler.scheduler import Scheduler
 
 CONSUMER_KEY = 'JLsalg6kGNWR3L9vdYg5ZesOW'
 CONSUMER_SECRET = '27xVJ8jDVuCvZz1jyxqIfFgbXv0GDD6q7axJJheQYbXFAnZUDn'
@@ -9,9 +8,7 @@ ACCESS_TOKEN = '3269501892-QeI4ToFqAPG2YeGumqPtKSwPhZrKL32uuUWAhxD'
 ACCESS_TOKEN_SECRET = 'uPq8hEHegCxx64w8RLsHJ2KnVhPG4ezC2nJ7xWmOfqxkI'
 
 api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-sched = Scheduler()
 
-@sched.interval_schedule(hours=1)
 def gen_tweet():
     if (random.randint(0,10)>2):
         return
@@ -29,6 +26,4 @@ def gen_tweet():
     output=output+'.'
     twit = api.request('statuses/update', {'status': output})
 
-
-#sched.configure(options_from_ini_file)
-sched.start()
+gen_tweet()
